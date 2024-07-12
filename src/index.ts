c5c83e78d399ac66842e537bda8f43884c1e2f9a
Vida Xie <vida_2020@163.com>
2024-07-13 00:32:14 +0800

    feat: support flat config
    chore: generate rules by scripts
    chore: run `lint:js`

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,14 @@
-import requireSetupStoreProps, {
-  RULE_NAME as requireSetupStorePropsName
-} from './rules/require-setup-store-properties-export'
+import { RULE_NAME as requireSetupStorePropsName } from './rules/require-setup-store-properties-export'
+import { RULE_NAME as neverExportInitializedStoreName } from './rules/never-export-initialized-store'
+import { RULE_NAME as preferNamingConventionName } from './rules/prefer-use-store-naming-convention'
+import { RULE_NAME as preferSingleStoreName } from './rules/prefer-single-store-per-file'
+import { RULE_NAME as noReturnGlobalPropertiesName } from './rules/no-return-global-properties'
+import { RULE_NAME as noDuplicateStoreIdsName } from './rules/no-duplicate-store-ids'
+import rules from './rules'
 
-import neverExportInitializedStore, {
-  RULE_NAME as neverExportInitializedStoreName
-} from './rules/never-export-initialized-store'
-
-import preferNamingConvention, {
-  RULE_NAME as preferNamingConventionName
-} from './rules/prefer-use-store-naming-convention'
-
-import preferSingleStore, {
-  RULE_NAME as preferSingleStoreName
-} from './rules/prefer-single-store-per-file'
-
-import noReturnGlobalProperties, {
-  RULE_NAME as noReturnGlobalPropertiesName
-} from './rules/no-return-global-properties'
-
-import noDuplicateStoreIds, {
-  RULE_NAME as noDuplicateStoreIdsName
-} from './rules/no-duplicate-store-ids'
-
-const createConfig = (rules: Record<string, string>) => ({
-  plugins: ['pinia'],
-  rules: Object.keys(rules).reduce((acc, ruleName) => {
-    return {
-      ...acc,
-      [`pinia/${ruleName}`]: rules[ruleName]
-    }
-  }, {})
-})
+const plugin = {
+  rules
+}
 
 const allRules = {
   [requireSetupStorePropsName]: 'warn',
@@ -48,17 +26,34 @@ const recommended = {
   [neverExportInitializedStoreName]: 'error'
 }
 
-export default {
-  rules: {
-    [requireSetupStorePropsName]: requireSetupStoreProps,
-    [neverExportInitializedStoreName]: neverExportInitializedStore,
-    [preferNamingConventionName]: preferNamingConvention,
-    [preferSingleStoreName]: preferSingleStore,
-    [noReturnGlobalPropertiesName]: noReturnGlobalProperties,
-    [noDuplicateStoreIdsName]: noDuplicateStoreIds
-  },
-  configs: {
-    all: createConfig(allRules),
-    recommended: createConfig(recommended)
+function createConfig<T extends Record<string, any>>(_rules: T, flat = false) {
+  const name = 'pinia'
+  const rules: Record<`pinia/${string}`, string> = Object.keys(_rules).reduce((acc, ruleName) => {
+    return {
+      ...acc,
+      [`${name}/${ruleName}`]: rules[ruleName]
+    }
+  }, {})
+  if (flat) {
+    return {
+      plugins: {
+        [name]: plugin
+      },
+      rules
+    }
+  } else {
+    return {
+      plugins: [name],
+      rules
+    }
   }
 }
+
+export const configs = {
+  all: createConfig(allRules),
+  recommended: createConfig(recommended),
+  'all-flat': createConfig(allRules, true),
+  'recommended-flat': createConfig(recommended, true)
+}
+
+export default plugin
--- src/rules/no-return-global-properties.ts
+++ src/rules/no-return-global-properties.ts
@@ -1,4 +1,3 @@
-import {} from 'typescript'
 import { createEslintRule } from '../utils/rule-creator'
 
 export const RULE_NAME = 'no-return-global-properties'
@@ -28,9 +27,8 @@ export default createEslintRule<Options, MESSAGE_IDS>({
         node.declarations.forEach((declaration) => {
           if (declaration.init && declaration.init.type === 'CallExpression') {
             const calleeName = declaration.init.callee.name
-            if (calleeName === 'useRoute' || calleeName === 'inject') {
+            if (calleeName === 'useRoute' || calleeName === 'inject')
               variablesUsingGlobalCallee.add(declaration.id.name)
-            }
           }
         })
       },
